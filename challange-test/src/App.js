9728fc111c2f3f617efc4b5486f3c1bee1d68720
raihanbadruz <raihanzaman79@gmail.com>
2023-10-03 21:36:40 +0700

    before navbar

--- challange-test/src/App.js
+++ challange-test/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import { getMovieList, searchMovie } from "./api";
 import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
 const App = () => {
   const [popularMovies, setPopularMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState();
 
   useEffect(() => {
     getMovieList().then((result) => {
@@ -12,6 +15,10 @@ const App = () => {
   }, []);
 
   const PopularMovieList = () => {
+    const handleMovieClick = (movie) => {
+      setSelectedMovie(movie);
+    };
+
     return popularMovies.map((movie) => {
       return (
         <div className="movie-wrapper" key={movie.id}>
@@ -19,12 +26,29 @@ const App = () => {
           <img
             className="movie-image"
             src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+            onClick={() => handleMovieClick(movie)}
           />
-          <div className="movie-date">Release: {movie.release_date}</div>
-          <div className="movie-rate">{movie.vote_average}</div>
         </div>
       );
     });
+  };
+
+  const MovieDetail = ({ movie, onClose }) => {
+    return (
+      <div className="movie-detail">
+        <div className="movie-title">{movie.title}</div>
+        <img
+          className="movie-image"
+          src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+        />
+        <div className="movie-date">Release: {movie.release_date}</div>
+        <div className="movie-rate">{movie.vote_average}</div>
+        <div className="movie-overview">{movie.overview}</div>
+        <button className="close-button" onClick={onClose}>
+          Close
+        </button>
+      </div>
+    );
   };
 
   const search = async (q) => {
@@ -46,7 +70,14 @@ const App = () => {
           onChange={({ target }) => search(target.value)}
         />
         <div className="movie-container">
-          <PopularMovieList />
+          {selectedMovie ? (
+            <MovieDetail
+              movie={selectedMovie}
+              onClose={() => setSelectedMovie(null)}
+            />
+          ) : (
+            <PopularMovieList />
+          )}
         </div>
       </header>
     </div>
