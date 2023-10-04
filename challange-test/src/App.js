e67fb067287bf41072f2f22cf6a62858c3496221
raihanbadruz <raihanzaman79@gmail.com>
2023-10-04 15:47:11 +0700

    90%

--- challange-test/src/App.js
+++ challange-test/src/App.js
@@ -34,19 +34,23 @@ const App = () => {
   };
 
   const MovieDetail = ({ movie, onClose }) => {
+    const backgroundImageStyle = {
+      backgroundImage: `url(${process.env.REACT_APP_BASEIMGURL}/${movie.backdrop_path})`,
+    };
+
     return (
-      <div className="movie-detail">
-        <div className="movie-title">{movie.title}</div>
-        <img
-          className="movie-image"
-          src={`${process.env.REACT_APP_BASEIMGURL}/${movie.backdrop_path}`}
-        />
-        <div className="movie-date">Release: {movie.release_date}</div>
-        <div className="movie-rate">{movie.vote_average.toFixed(1)}</div>
-        <div className="movie-overview">{movie.overview}</div>
-        <button className="close-button" onClick={onClose}>
-          Close
-        </button>
+      <div className="movie-detail" style={backgroundImageStyle}>
+        <div className="content-overlay">
+          <div className="movie-title">{movie.title}</div>
+          <div className="movie-overview">{movie.overview}</div>
+          <div className="movie-date">Release : {movie.release_date}</div>
+          <div className="movie-rate">
+            Rating : {movie.vote_average.toFixed(2)}
+          </div>
+          <button className="close-button" onClick={onClose}>
+            Close
+          </button>
+        </div>
       </div>
     );
   };
