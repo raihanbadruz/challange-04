3c76fa3687b2bd6609536d2d4e6d65033af5aba4
raihanbadruz <raihanzaman79@gmail.com>
2023-10-04 15:52:40 +0700

    insyallah submit

--- exercise1/src/api/index.jsx
+++
@@ -1,9 +0,0 @@
-//import axios
-import axios from "axios";
-
-const Api = axios.create({
-  //set default endpoint API
-  baseURL: "https://pokeapi.co/api/v2/pokemon/ditto",
-});
-
-export default Api;
--- exercise2/src/api/index.jsx
+++
@@ -1,9 +0,0 @@
-//import axios
-import axios from "axios";
-
-const Api = axios.create({
-  //set default endpoint API
-  baseURL: "https://cron.eternityinvitation.com/users",
-});
-
-export default Api;
