386dfb2862746a40cf7fcdcd35a7daa4b0edc828
raihanbadruz <raihanzaman79@gmail.com>
2023-10-04 14:42:07 +0700

    kurang detail

--- challange-test/src/App.js
+++ challange-test/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { getMovieList, searchMovie } from "./api";
 import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
+import { Navbar, Button } from "react-bootstrap";
 
 const App = () => {
   const [popularMovies, setPopularMovies] = useState([]);
@@ -39,10 +39,10 @@ const App = () => {
         <div className="movie-title">{movie.title}</div>
         <img
           className="movie-image"
-          src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+          src={`${process.env.REACT_APP_BASEIMGURL}/${movie.backdrop_path}`}
         />
         <div className="movie-date">Release: {movie.release_date}</div>
-        <div className="movie-rate">{movie.vote_average}</div>
+        <div className="movie-rate">{movie.vote_average.toFixed(1)}</div>
         <div className="movie-overview">{movie.overview}</div>
         <button className="close-button" onClick={onClose}>
           Close
@@ -62,18 +62,26 @@ const App = () => {
 
   return (
     <div className="App">
+      <div className="navbar">
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand className="logo">Movielist</Navbar.Brand>
+            <input
+              placeholder="Search..."
+              className="search-movie"
+              onChange={({ target }) => search(target.value)}
+            />
+            <Button className="login-button">Login</Button>
+            <Button className="register-button">Register</Button>
+          </Container>
+        </Navbar>
+      </div>
       <header className="App-header">
-        <h1>JORDI MOVIE</h1>
-        <input
-          placeholder="search..."
-          className="search-movie"
-          onChange={({ target }) => search(target.value)}
-        />
         <div className="movie-container">
           {selectedMovie ? (
             <MovieDetail
               movie={selectedMovie}
-              onClose={() => setSelectedMovie(null)}
+              onClose={() => setSelectedMovie()}
             />
           ) : (
             <PopularMovieList />
